86c8fd2beb74e1ae6dd7274418816b687227ad65
Helena <hgarcia@es.amnesty.org>
2016-01-26 12:41:43 +0100

    added js

--- js/socixs-form.js
+++ js/socixs-form.js
@@ -1,4 +1,6 @@
-jQuery(function() {
+jQuery(function($) {
+
+    // Scrolling the active block of fields
 
     $(".content-colaborar").hover( function(){
         $(".caja-content").removeClass('active');
@@ -47,4 +49,31 @@ jQuery(function() {
 
     });    
 
-});
+
+  // Make the IBAN fields to automatically move the cursor through when any field is fullfilled.
+  
+  $(".country").keyup( function(){
+    if($(".country").val().length >= 2){
+        $(".entity").focus();        
+    }
+  });
+
+  $(".entity").keyup( function(){
+    if($(".entity").val().length >= 4){
+        $(".office").focus();        
+    }
+  });
+
+  $(".office").keyup( function(){
+    if($(".office").val().length >= 4){
+        $(".check").focus();        
+    }
+  });
+
+  $(".check").keyup( function(){
+    if($(".check").val().length >= 2){
+        $(".account").focus();        
+    }
+  });
+
+})
