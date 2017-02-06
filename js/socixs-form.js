8fc23439ff2e57b6f6315201fd8f32e6623b108e
Helena <hgarcia@es.amnesty.org>
2017-02-06 18:24:43 +0100

    #RM:1804 Añadido rellenado de campos para subida de cuota

--- js/socixs-form.js
+++ js/socixs-form.js
@@ -30,7 +30,7 @@ jQuery(function($) {
         source_input.val(get_source);
 
         // contrib_source if exists
-        if( isset("[name='submitted[civicrm_1_contribution_1_cg21_custom_104]']") ){
+        if( $("[name='submitted[civicrm_1_contribution_1_cg21_custom_104]']") ){
           var contrib_source_input = $( "[name='submitted[civicrm_1_contribution_1_cg21_custom_104]']");
           contrib_source_input.val(get_source);
         }
