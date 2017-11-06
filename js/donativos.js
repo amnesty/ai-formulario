257d287f0d77bee318d1474af1a503f597505a5c
frodriguez <frodriguez@es.amnesty.org>
2017-11-06 16:10:54 +0100

    Se añade suborigen a la entrada de datos

--- js/donativos.js
+++ js/donativos.js
@@ -16,8 +16,10 @@ jQuery(function($) {
     var get_source = getUrlVars()["origen"];
     var get_campaign = getUrlVars()["pk_campaign"];
     var get_kw = getUrlVars()["pk_kwd"];
+    var get_suborigen = getUrlVars()["caso"];
     var contrib_source_input = $( "[name='submitted[civicrm_1_contribution_1_cg21_custom_104]']");
     var contrib_kw_input = $( "[name='submitted[civicrm_1_contribution_1_cg21_custom_103]']");
+    var contrib_suborigen_input = $( "[name='submitted[civicrm_1_contribution_1_cg21_custom_109]']");
 
     // origen
     if( get_source != '' && get_source ){ // origen
@@ -35,6 +37,9 @@ jQuery(function($) {
     // pk_kwd
     if( get_kw != '' && get_kw ){
         contrib_kw_input.val(get_kw);
+    }
+    else if( get_suborigen != '' && get_suborigen ){ //suborigen
+            contrib_suborigen_input.val(get_suborigen);
     }
 
     // Esconder otra cuota
