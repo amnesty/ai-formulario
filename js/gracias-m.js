1e6051c47ad21943ec16d528d8ed9606aa9d30de
frodriguez <frodriguez@es.amnesty.org>
2018-02-21 13:20:34 +0100

    Corrección script gracias-m.js

--- js/gracias-m.js
+++ js/gracias-m.js
@@ -12,21 +12,6 @@ function $_GET(param) {
   return vars;
 }
 
-function Array(param) {
-  var vars = {};
-  window.location.href.replace( location.hash, '' ).replace(
-    /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
-    function( m, key, value ) { // callback
-      vars[key] = value !== undefined ? value : '';
-    }
-  );
-  if ( param ) {
-    return vars[param] ? vars[param] : null;
-  }
-  return vars;
-}
-
-
 jQuery(function($) {
   var cd1 = $_GET("cid1");
   $('.form-actions').show();
