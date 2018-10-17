8ad355354247f5bd85665aa62caa02f18a8e5ef8
Helena <hgarcia@es.amnesty.org>
2018-10-17 17:03:05 +0200

    trackear pk_content

--- js/socixs-form.js
+++ js/socixs-form.js
@@ -19,13 +19,14 @@ jQuery(function($) {
     var get_kwd = getUrlVars()["pk_kwd"];
     var get_utm_medium = getUrlVars()["utm_medium"];
     var get_utm_source = getUrlVars()["utm_source"];
+    var get_content = getUrlVars()["pk_content"];
     var get_utm_content = getUrlVars()["utm_content"];
     var get_utm_term = getUrlVars()["utm_term"];
     var get_suborigen = getUrlVars()["caso"];
 
-    var source_input = $( "[name='submitted[civicrm_1_contact_1_contact_source]']");
-    var source_input2 = $( "[name='submitted[civicrm_1_contact_1_cg17_custom_50]']");
-    var suborigen_input = $( "[name='submitted[civicrm_1_contact_1_cg17_custom_110]']");
+    var source_input = $("[name='submitted[civicrm_1_contact_1_contact_source]']");
+    var source_input2 = $("[name='submitted[civicrm_1_contact_1_cg17_custom_50]']");
+    var suborigen_input = $("[name='submitted[civicrm_1_contact_1_cg17_custom_110]']");
 
     // origen
     if( get_source != '' && get_source ){ // origen
@@ -64,7 +65,10 @@ jQuery(function($) {
     }
     // utm-content
     var source_input = $( "[name='submitted[civicrm_1_contact_1_cg17_custom_53]']");
-    if( get_utm_content != '' && get_utm_content ){
+    if( get_content != '' && get_content ){
+        source_input.val(get_content);
+    }
+    else if( get_utm_content != '' && get_utm_content ){
         source_input.val(get_utm_content);
     }
     // utm-term
