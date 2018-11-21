e9ae2657664196ddd0033f25d5e1b0cfa1e55d8b
frodriguez <frodriguez@es.amnesty.org>
2018-11-21 12:44:43 +0100

    Cambios js cuotas

--- js/cuotas.js
+++ js/cuotas.js
@@ -1,80 +1,157 @@
 /* JS especifico para la ponderación de las cantidades de las cuotas en función de la periodicidad*/
 
 jQuery(function($) {
-$(".frecuencia").change( function() {
 
   $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-1").addClass("cuota-1");
   $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-2").addClass("cuota-2");
   $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-3").addClass("cuota-3");
+  $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-4").addClass("cuota-4");
+  $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-5").addClass("cuota-5");
+  $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-6").addClass("cuota-6");
+  $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-7").addClass("cuota-7");
+  $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-8").addClass("cuota-8");
+  $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-9").addClass("cuota-9");
+  $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-10").addClass("cuota-10");
+  $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-11").addClass("cuota-11");
+  $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-12").addClass("cuota-12");
+  $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-13").addClass("cuota-13");
+  $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-14").addClass("cuota-14");
+  $("#edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-15").addClass("cuota-15");
 
-  var cuota_1 = $(".cuota-1");
-  var cuota_2 = $(".cuota-2");
-  var cuota_3 = $(".cuota-3");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-1]").addClass("cuota-1");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-2]").addClass("cuota-2");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-3]").addClass("cuota-3");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-4]").addClass("cuota-4");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-5]").addClass("cuota-5");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-6]").addClass("cuota-6");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-7]").addClass("cuota-7");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-8]").addClass("cuota-8");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-9]").addClass("cuota-9");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-10]").addClass("cuota-10");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-11]").addClass("cuota-11");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-12]").addClass("cuota-12");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-13]").addClass("cuota-13");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-14]").addClass("cuota-14");
+  $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-15]").addClass("cuota-15");
 
-  var cuota_1_text = $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-1]");
-  var cuota_2_text = $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-2]");
-  var cuota_3_text = $("label[for=edit-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48-3]");
+  $('.cuota-1').show();
+  $('.cuota-2').show();
+  $('.cuota-3').show();
+  $('.cuota-4').hide();
+  $('.cuota-5').hide();
+  $('.cuota-6').hide();
+  $('.cuota-7').hide();
+  $('.cuota-8').hide();
+  $('.cuota-9').hide();
+  $('.cuota-10').hide();
+  $('.cuota-11').hide();
+  $('.cuota-12').hide();
+  $('.cuota-13').hide();
+  $('.cuota-14').hide();
+  $('.cuota-15').hide();
 
-  cuota_1.addClass("cuota-1");
-  cuota_2.addClass("cuota-2");
-  cuota_3.addClass("cuota-3");
+  $(".frecuencia").change( function() {
 
     switch($(".frecuencia").val()){
       case '12': //Mensual
-        //Value
-        cuota_1.val(9);
-        cuota_2.val(12);
-        cuota_3.val(25);
-        //Texto
-        cuota_1_text.text('9 €');
-        cuota_2_text.text('12 €');
-        cuota_3_text.text('25 €');
-        break;
+
+      $('.cuota-1').show();
+      $('.cuota-2').show();
+      $('.cuota-3').show();
+      $('.cuota-4').hide();
+      $('.cuota-5').hide();
+      $('.cuota-6').hide();
+      $('.cuota-7').hide();
+      $('.cuota-8').hide();
+      $('.cuota-9').hide();
+      $('.cuota-10').hide();
+      $('.cuota-11').hide();
+      $('.cuota-12').hide();
+      $('.cuota-13').hide();
+      $('.cuota-14').hide();
+      $('.cuota-15').hide();
+
+      break;
 
       case '6': //Bimestral
-        //Value
-        cuota_1.val(15);
-        cuota_2.val(24);
-        cuota_3.val(40);
-        //Texto
-        cuota_1_text.text('15 €');
-        cuota_2_text.text('24 €');
-        cuota_3_text.text('40 €');
-        break;
+
+      $('.cuota-1').hide();
+      $('.cuota-2').hide();
+      $('.cuota-3').hide();
+      $('.cuota-4').show();
+      $('.cuota-5').show();
+      $('.cuota-6').show();
+      $('.cuota-7').hide();
+      $('.cuota-8').hide();
+      $('.cuota-9').hide();
+      $('.cuota-10').hide();
+      $('.cuota-11').hide();
+      $('.cuota-12').hide();
+      $('.cuota-13').hide();
+      $('.cuota-14').hide();
+      $('.cuota-15').hide();
+
+      break;
 
       case '4': //Trimestral
-        //Value
-        cuota_1.val(20);
-        cuota_2.val(36);
-        cuota_3.val(50);
-        //Texto
-        cuota_1_text.text('20 €');
-        cuota_2_text.text('36 €');
-        cuota_3_text.text('50 €');
-        break;
+
+      $('.cuota-1').hide();
+      $('.cuota-2').hide();
+      $('.cuota-3').hide();
+      $('.cuota-4').hide();
+      $('.cuota-5').hide();
+      $('.cuota-6').hide();
+      $('.cuota-7').show();
+      $('.cuota-8').show();
+      $('.cuota-9').show();
+      $('.cuota-10').hide();
+      $('.cuota-11').hide();
+      $('.cuota-12').hide();
+      $('.cuota-13').hide();
+      $('.cuota-14').hide();
+      $('.cuota-15').hide();
+
+      break;
 
       case '2': //Semestral
-        //Value
-        cuota_1.val(45);
-        cuota_2.val(60);
-        cuota_3.val(100);
-        //Texto
-        cuota_1_text.text('45 €');
-        cuota_2_text.text('60 €');
-        cuota_3_text.text('100 €');
-        break;
+
+      $('.cuota-1').hide();
+      $('.cuota-2').hide();
+      $('.cuota-3').hide();
+      $('.cuota-4').hide();
+      $('.cuota-5').hide();
+      $('.cuota-6').hide();
+      $('.cuota-7').hide();
+      $('.cuota-8').hide();
+      $('.cuota-9').hide();
+      $('.cuota-10').show();
+      $('.cuota-11').show();
+      $('.cuota-12').show();
+      $('.cuota-13').hide();
+      $('.cuota-14').hide();
+      $('.cuota-15').hide();
+
+      break;
 
       case '1': //Anual
-        //Value
-        cuota_1.val(70);
-        cuota_2.val(100);
-        cuota_3.val(120);
-        //Texto
-        cuota_1_text.text('70 €');
-        cuota_2_text.text('100 €');
-        cuota_3_text.text('120 €');
-        break;
+      $('.cuota-1').hide();
+      $('.cuota-2').hide();
+      $('.cuota-3').hide();
+      $('.cuota-4').hide();
+      $('.cuota-5').hide();
+      $('.cuota-6').hide();
+      $('.cuota-7').hide();
+      $('.cuota-8').hide();
+      $('.cuota-9').hide();
+      $('.cuota-10').hide();
+      $('.cuota-11').hide();
+      $('.cuota-12').hide();
+      $('.cuota-13').show();
+      $('.cuota-14').show();
+      $('.cuota-15').show();
+
+      break;
     }
-});
+  });
 
 });
