90bc2120a2957b2673fbd51f9c2fffa16ccbc486
Helena <hgarcia@es.amnesty.org>
2018-12-05 16:45:01 +0100

    agrupado cuotas.js en funciones e inicializados valores para que dependan de la periodicidad seleccionada, por defecto si es la primera vez, o la ultima conocida si el origen es la página de confirmacion o un usuario logueado

--- js/cuotas.js
+++ js/cuotas.js
@@ -1,7 +1,98 @@
-/* JS especifico para la ponderación de las cantidades de las cuotas en función de la periodicidad*/
+/* JS especifico para la ponderación de las cantidades de las cuotas en función de la periodicidad */
 
 jQuery(function($) {
 
+  function valores_mensuales(){
+    $('.cuota-1').show();
+    $('.cuota-2').show();
+    $('.cuota-3').show();
+    $('.cuota-4').hide();
+    $('.cuota-5').hide();
+    $('.cuota-6').hide();
+    $('.cuota-7').hide();
+    $('.cuota-8').hide();
+    $('.cuota-9').hide();
+    $('.cuota-10').hide();
+    $('.cuota-11').hide();
+    $('.cuota-12').hide();
+    $('.cuota-13').hide();
+    $('.cuota-14').hide();
+    $('.cuota-15').hide();
+  }
+
+  function valores_trimestrales(){
+    $('.cuota-1').hide();
+    $('.cuota-2').hide();
+    $('.cuota-3').hide();
+    $('.cuota-4').hide();
+    $('.cuota-5').hide();
+    $('.cuota-6').hide();
+    $('.cuota-7').show();
+    $('.cuota-8').show();
+    $('.cuota-9').show();
+    $('.cuota-10').hide();
+    $('.cuota-11').hide();
+    $('.cuota-12').hide();
+    $('.cuota-13').hide();
+    $('.cuota-14').hide();
+    $('.cuota-15').hide();
+  }
+
+  function valores_bimestrales(){
+    $('.cuota-1').hide();
+    $('.cuota-2').hide();
+    $('.cuota-3').hide();
+    $('.cuota-4').show();
+    $('.cuota-5').show();
+    $('.cuota-6').show();
+    $('.cuota-7').hide();
+    $('.cuota-8').hide();
+    $('.cuota-9').hide();
+    $('.cuota-10').hide();
+    $('.cuota-11').hide();
+    $('.cuota-12').hide();
+    $('.cuota-13').hide();
+    $('.cuota-14').hide();
+    $('.cuota-15').hide();
+  }
+
+  function valores_semestrales(){
+    $('.cuota-1').hide();
+    $('.cuota-2').hide();
+    $('.cuota-3').hide();
+    $('.cuota-4').hide();
+    $('.cuota-5').hide();
+    $('.cuota-6').hide();
+    $('.cuota-7').hide();
+    $('.cuota-8').hide();
+    $('.cuota-9').hide();
+    $('.cuota-10').show();
+    $('.cuota-11').show();
+    $('.cuota-12').show();
+    $('.cuota-13').hide();
+    $('.cuota-14').hide();
+    $('.cuota-15').hide();
+  }
+
+  function valores_anuales(){
+    $('.cuota-1').hide();
+    $('.cuota-2').hide();
+    $('.cuota-3').hide();
+    $('.cuota-4').hide();
+    $('.cuota-5').hide();
+    $('.cuota-6').hide();
+    $('.cuota-7').hide();
+    $('.cuota-8').hide();
+    $('.cuota-9').hide();
+    $('.cuota-10').hide();
+    $('.cuota-11').hide();
+    $('.cuota-12').hide();
+    $('.cuota-13').show();
+    $('.cuota-14').show();
+    $('.cuota-15').show();
+  }
+
+  // Al inicializar, añadimos a cada cuota con un identificador cuota-x
   var class_id = 1;
    var num_cuotas = $(".form-item-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48").length;
    $(".form-item-submitted-caja-colaborar-fieldset-fila-1-0-civicrm-1-contact-1-cg15-custom-48").each(function(){
@@ -13,149 +104,67 @@ jQuery(function($) {
      class_id++;
    });
 
+  // Al inicializar, vemos qué valores hay que poner según la periodicidad
+  switch( $(".frecuencia").val() ) {
+    case '12': //Mensual
+    valores_mensuales();
+    break;
 
-  //Por defecto marcamos 12€
-  /*$(".cuota-2").prop("checked",true);*/
+    case '6': //Bimestral
+    valores_bimestrales();
+    break;
 
-  // Por defecto mostramos solo las cuotas mensuales
-  $('.cuota-1').show();
-  $('.cuota-2').show();
-  $('.cuota-3').show();
-  $('.cuota-4').hide();
-  $('.cuota-5').hide();
-  $('.cuota-6').hide();
-  $('.cuota-7').hide();
-  $('.cuota-8').hide();
-  $('.cuota-9').hide();
-  $('.cuota-10').hide();
-  $('.cuota-11').hide();
-  $('.cuota-12').hide();
-  $('.cuota-13').hide();
-  $('.cuota-14').hide();
-  $('.cuota-15').hide();
+    case '4': //Trimestral
+    valores_trimestrales();
+    break;
 
+    case '2': //Semestral
+    valores_semestrales();
+    break;
+
+    case '1': //Anual
+    valores_anuales();
+    break;
+  }
+
+  /***** Cada vez que cambia la periodicidad, revisamos los valores *****/
   $(".frecuencia").change( function() {
 
     // Escondemos "otra cantidad" por si acaso porque por defecto marcamos la opción del medio
     $('.capa-other-quant').hide();
 
-    switch($(".frecuencia").val()){
+    switch( $(".frecuencia").val() ) {
       case '12': //Mensual
-
       //Valor por defecto
       $(".cuota-2").prop("checked",true);
-
-      $('.cuota-1').show();
-      $('.cuota-2').show();
-      $('.cuota-3').show();
-      $('.cuota-4').hide();
-      $('.cuota-5').hide();
-      $('.cuota-6').hide();
-      $('.cuota-7').hide();
-      $('.cuota-8').hide();
-      $('.cuota-9').hide();
-      $('.cuota-10').hide();
-      $('.cuota-11').hide();
-      $('.cuota-12').hide();
-      $('.cuota-13').hide();
-      $('.cuota-14').hide();
-      $('.cuota-15').hide();
-
+      valores_mensuales();
       break;
 
       case '6': //Bimestral
-
       //Valor por defecto
       $(".cuota-5").prop("checked",true);
-
-      $('.cuota-1').hide();
-      $('.cuota-2').hide();
-      $('.cuota-3').hide();
-      $('.cuota-4').show();
-      $('.cuota-5').show();
-      $('.cuota-6').show();
-      $('.cuota-7').hide();
-      $('.cuota-8').hide();
-      $('.cuota-9').hide();
-      $('.cuota-10').hide();
-      $('.cuota-11').hide();
-      $('.cuota-12').hide();
-      $('.cuota-13').hide();
-      $('.cuota-14').hide();
-      $('.cuota-15').hide();
-
+      valores_bimestrales();
       break;
 
       case '4': //Trimestral
-
       //Valor por defecto
       $(".cuota-8").prop("checked",true);
-
-      $('.cuota-1').hide();
-      $('.cuota-2').hide();
-      $('.cuota-3').hide();
-      $('.cuota-4').hide();
-      $('.cuota-5').hide();
-      $('.cuota-6').hide();
-      $('.cuota-7').show();
-      $('.cuota-8').show();
-      $('.cuota-9').show();
-      $('.cuota-10').hide();
-      $('.cuota-11').hide();
-      $('.cuota-12').hide();
-      $('.cuota-13').hide();
-      $('.cuota-14').hide();
-      $('.cuota-15').hide();
-
+      valores_trimestrales();
       break;
 
       case '2': //Semestral
-
       //Valor por defecto
       $(".cuota-11").prop("checked",true);
-
-
-      $('.cuota-1').hide();
-      $('.cuota-2').hide();
-      $('.cuota-3').hide();
-      $('.cuota-4').hide();
-      $('.cuota-5').hide();
-      $('.cuota-6').hide();
-      $('.cuota-7').hide();
-      $('.cuota-8').hide();
-      $('.cuota-9').hide();
-      $('.cuota-10').show();
-      $('.cuota-11').show();
-      $('.cuota-12').show();
-      $('.cuota-13').hide();
-      $('.cuota-14').hide();
-      $('.cuota-15').hide();
-
+      valores_semestrales();
       break;
 
       case '1': //Anual
-
       //Valor por defecto
       $(".cuota-14").prop("checked",true);
-
-      $('.cuota-1').hide();
-      $('.cuota-2').hide();
-      $('.cuota-3').hide();
-      $('.cuota-4').hide();
-      $('.cuota-5').hide();
-      $('.cuota-6').hide();
-      $('.cuota-7').hide();
-      $('.cuota-8').hide();
-      $('.cuota-9').hide();
-      $('.cuota-10').hide();
-      $('.cuota-11').hide();
-      $('.cuota-12').hide();
-      $('.cuota-13').show();
-      $('.cuota-14').show();
-      $('.cuota-15').show();
-
+      valores_anuales();
       break;
     }
+
   });
 
 });
