5a6b2f63f39c34044aded407252640a6b82aa6d2
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2022-08-31 10:03:08 +0100

    Muestra la opción otra cuota vacía por defecto

--- js/cuotas-m.js
+++ js/cuotas-m.js
@@ -15,9 +15,12 @@
     }
   }
 
-  function cuotaSeleccionada() {
-      let frecuencia = frecuenciaSeleccionada();
-      return $(`.sugeridas_periodicidad_${frecuencia} input.cuota_sugerida:checked`).val();
+  function cuotaActual() {
+      let sugeridaSeleccionada = $("input.cuota_sugerida:visible:checked").val();
+
+      if (sugeridaSeleccionada != 0) {
+          return sugeridaSeleccionada;
+      }
   }
 
   function controlaCuotasSugeridas() {
@@ -25,37 +28,41 @@
       $(".cuotas").hide();
       $(`.sugeridas_periodicidad_${frecuencia}`).show();
   }
+
   function controlaOtraCuota() {
-    let cuota = cuotaSeleccionada();
+      let cuota = cuotaActual();
 
-    if (cuota == 0) {
-        $("input.otra_cuota").parent().show();
-    } else {
-        $("input.otra_cuota").val(cuota);
-        $("input.otra_cuota").parent().hide();
-    }
-}
+      if (!cuota) {
+          $("input.otra_cuota").val("");
+          $("input.otra_cuota").parent().show();
+      } else {
+          $("input.otra_cuota").val(cuota);
+          $("input.otra_cuota").parent().hide();
+      }
+  }
 
-$("input.cuota_sugerida").change(function() {
-    controlaOtraCuota();
+  $("input.cuota_sugerida").change(function() {
+      controlaOtraCuota();
+  });
+
+  if ($("select.frecuencia").length) {
+    frecuencia = $("select.frecuencia");
+  } else {
+    frecuencia = $("input.frecuencia");
+  }
+
+  frecuencia.change(function() {
+      controlaCuotasSugeridas();
+      controlaOtraCuota();
+  });
+
+  controlaCuotasSugeridas();
+  controlaOtraCuota();
+
+  frecuencia.closest("form")
+      .addClass("webform-conditional-processed");
+
+  frecuencia.closest("form").parent()
+      .addClass("content-form");
 });
-if ($("select.frecuencia").length) {
-  frecuencia = $("select.frecuencia");
-} else {
-  frecuencia = $("input.frecuencia");
-}
 
-frecuencia.change(function() {
-    controlaCuotasSugeridas();
-    controlaOtraCuota();
-});
-
-controlaCuotasSugeridas();
-controlaOtraCuota();
-
-frecuencia.closest("form")
-    .addClass("webform-conditional-processed");
-
-frecuencia.closest("form").parent()
-    .addClass("content-form");
-});
