f5bddc876ebd5bc9bca69ae59eb826a1c4294f1e
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2022-12-29 12:11:57 +0000

    Fix hover not working in some cases

--- js/socixs-form.js
+++ js/socixs-form.js
@@ -132,57 +132,47 @@ jQuery(function($) {
 
     // Scrolling the active block of fields
 
-    if( $('.webform-client-form').hasClass('webform-conditional-processed') ){
+    $(".content-colaborar").hover( function(){
+        $(".caja-content").removeClass('active');
+        $(this).addClass('active');
+    });
 
-        $(".content-colaborar").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+    $(".content-colaborar").keyup( function(){
+        $(".caja-content").removeClass('active');
+        $(this).addClass('active');
+    });
 
-        });
+    $(".content-datos").hover( function(){
+        $(".caja-content").removeClass('active');
+        $(this).addClass('active');
+    });
 
-        $(".content-colaborar").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+    $(".content-datos").keyup( function(){
+        $(".caja-content").removeClass('active');
+        $(this).addClass('active');
+    });
 
-        });
+    $(".content-direccion").hover( function(){
+        $(".caja-content").removeClass('active');
+        $(this).addClass('active');
+    });
 
-        $(".content-datos").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+    $(".content-direccion").keyup( function(){
+        $(".caja-content").removeClass('active');
+        $(this).addClass('active');
+    });
 
-        });
+    $(".content-cuenta").hover( function(){
+        $(".caja-content").removeClass('active');
+        $(this).addClass('active');
+    });
 
-        $(".content-datos").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
+    $(".content-cuenta").keyup( function(){
+        $(".caja-content").removeClass('active');
+        $(this).addClass('active');
+    });
 
-        });
-
-        $(".content-direccion").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
-
-        });
-
-        $(".content-direccion").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
-        });
-
-        $(".content-cuenta").hover( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
-
-        });
-
-        $(".content-cuenta").keyup( function(){
-            $(".caja-content").removeClass('active');
-            $(this).addClass('active');
-
-        });
-
-    }
-    else {
+    if( ! $('.webform-client-form').hasClass('webform-conditional-processed') ){
         $(".caja-content").removeClass('active');
     }
 
